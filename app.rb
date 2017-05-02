21a18e3d1eed26bbcbbdb8cc0e53113588301fc5
Greg Horne <greghorne@hotmail.com>
2017-05-02 22:15:28 +0000

    added EventSource support check

--- app.rb
+++ app.rb
@@ -230,8 +230,8 @@ __END__
         z-index: 1;
         top: 0;
         left: 0;
-        /*background-color: rgb(241,241,241);*/
-        background-color: rgba(181,152,152, 0.8);
+        background-color: rgba(46, 113, 197, 0.6);
+        /*background-color: rgba(181,152,152, 0.8);*/
         overflow-x: hidden;
         transition: 1s;
         font-family: 'Lato', sans-serif;
@@ -288,158 +288,162 @@ __END__
 
 <script>
 
-  var marker;
-  var trace = true; 
+  if (typeof(EventSource) !== undefined) {
+    var marker;
+    var trace = true; 
 
-  $(document).ready(function() {
+    $(document).ready(function() {
 
-    var eventSource = new EventSource('/streamer');
+      var eventSource = new EventSource('/streamer');
 
-    // =======================================
-    // event - message =======================
-    // =======================================
-    eventSource.addEventListener('message', function(event){
+      // =======================================
+      // event - message =======================
+      // =======================================
+      eventSource.addEventListener('message', function(event){
 
-      var divText = "";
+        var divText = "";
 
-      json = JSON.parse(event.data);
-      if (json['msg'] != "0") {
-        
-        if (trace) console.log(json);
+        json = JSON.parse(event.data);
+        if (json['msg'] != "0") {
+          
+          if (trace) console.log(json);
 
-        // extract some data from json object
-        longitudeX = json['x'];
-        latitudeY  = json['y'];
-        msg        = json['msg'];
-        depth      = json['z'];
-        time       = json['utc'];
+          // extract some data from json object
+          longitudeX = json['x'];
+          latitudeY  = json['y'];
+          msg        = json['msg'];
+          depth      = json['z'];
+          time       = json['utc'];
 
-        // if any, remove current map marker
-        if (marker !== undefined) map.removeLayer(marker);
+          // if any, remove current map marker
+          if (marker !== undefined) map.removeLayer(marker);
 
-        // add new marker to map
-        marker = L.marker([latitudeY, longitudeX]).addTo(map);
+          // add new marker to map
+          marker = L.marker([latitudeY, longitudeX]).addTo(map);
 
-        // add marker popup informaiton and open popup window
-        marker.bindPopup("<center>" + msg + "<br>Depth: " + depth + " km<br>UTC: " + time + "<center>").openPopup();
+          // add marker popup informaiton and open popup window
+          marker.bindPopup("<center>" + msg + "<br>Depth: " + depth + " km<br>UTC: " + time + "<center>").openPopup();
 
-        // pan and zoom to new map (earthquake) location
-        map.setView(L.latLng(latitudeY, longitudeX), 8);
+          // pan and zoom to new map (earthquake) location
+          map.setView(L.latLng(latitudeY, longitudeX), 8);
 
-        // $("#msg").text("");
-        divText = "";
+          // $("#msg").text("");
+          divText = "";
 
-        $("#map").effect("shake", "times: 20");
+          $("#map").effect("shake", "times: 20");
 
-      } else {
-        if (trace) console.log(json);
+        } else {
+          if (trace) console.log(json);
 
-        var count = json["usgs earthquake count"]
+          var count = json["usgs earthquake count"]
 
-        if (count === 0) $("#msg").css('color', 'rgb(165,255,144)')
-        else if (count < 6) $("#msg").css('color', 'yellow')
-        else $("#msg").css('color', 'rgb(245,107,97)')
+          if (count === 0) $("#msg").css('color', 'rgb(165,255,144)')
+          else if (count < 6) $("#msg").css('color', 'yellow')
+          else $("#msg").css('color', 'rgb(245,107,97)')
 
-        var date = new Date();
-        json['msg'] = date;
+          var date = new Date();
+          json['msg'] = date;
 
-        divText = date + " === USGS Earthquake Count: " + json["usgs earthquake count"] + " (last " + json["in recent minutes"] + " minutes)"
+          divText = date + " === USGS Earthquake Count: " + json["usgs earthquake count"] + " (last " + json["in recent minutes"] + " minutes)"
 
+        }
+        $("#msg").text(divText);
+
+      }, false);
+      // =======================================
+
+
+      // =======================================
+      // event - error =========================
+      // =======================================
+      eventSource.addEventListener('error', function(event) {
+        // if this event fires it will automatically try and reconnect
+        if (trace) {
+          console.log("--------------------")
+          console.log("error...")
+          console.log(event)
+          console.log("--------------------")
+        }
+      }, false);
+      // =======================================
+
+
+      // =======================================
+      // event - close =========================
+      // =======================================
+      eventSource.addEventListener('close', function(event) {
+        if (trace) {
+          console.log("--------------------")
+          console.log("connection closed...")
+          console.log(event)
+          console.log("--------------------")
+        }
+      }, false)
+      // =======================================
+
+
+      // =======================================
+      // leaflet begin >>>>>>>>>>>>>>>>>>>>>>>>>
+      // =======================================
+
+      // initial map settings/view
+      var latitude = 35.746512259918504
+      var longitude = -96.9873046875
+      var zoom = 4
+
+      // kudos
+      var attributionOSM = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+
+      var osm = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {  
+        attribution: attributionOSM,
+        subdomains: ['a', 'b', 'c']
+      });
+
+      var map = L.map('map', {
+        center: [latitude, longitude],
+        zoom: zoom,
+        layers: [osm]
+      });
+
+      // scalebar
+      L.control.scale().addTo(map);
+
+      L.Control.Help = L.Control.extend({
+          onAdd: function(map) {
+
+              var img = L.DomUtil.create('img');
+
+              img.src = '/images/help1.png';
+              img.addEventListener('click', function () {
+                openNav();
+                console.log("open nav...")
+              })
+
+              return img;
+          },
+      });
+
+      L.control.help = function(opts) {
+          return new L.Control.Help(opts);
       }
-      $("#msg").text(divText);
 
-    }, false);
-    // =======================================
+      L.control.help({ position: 'topright' }).addTo(map);
 
-
-    // =======================================
-    // event - error =========================
-    // =======================================
-    eventSource.addEventListener('error', function(event) {
-      // if this event fires it will automatically try and reconnect
-      if (trace) {
-        console.log("--------------------")
-        console.log("error...")
-        console.log(event)
-        console.log("--------------------")
-      }
-    }, false);
-    // =======================================
-
-
-    // =======================================
-    // event - close =========================
-    // =======================================
-    eventSource.addEventListener('close', function(event) {
-      if (trace) {
-        console.log("--------------------")
-        console.log("connection closed...")
-        console.log(event)
-        console.log("--------------------")
-      }
-    }, false)
-    // =======================================
-
-
-    // =======================================
-    // leaflet begin >>>>>>>>>>>>>>>>>>>>>>>>>
-    // =======================================
-
-    // initial map settings/view
-    var latitude = 35.746512259918504
-    var longitude = -96.9873046875
-    var zoom = 4
-
-    // kudos
-    var attributionOSM = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-
-    var osm = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {  
-      attribution: attributionOSM,
-      subdomains: ['a', 'b', 'c']
     });
 
-    var map = L.map('map', {
-      center: [latitude, longitude],
-      zoom: zoom,
-      layers: [osm]
-    });
-
-    // scalebar
-    L.control.scale().addTo(map);
-
-    L.Control.Help = L.Control.extend({
-        onAdd: function(map) {
-
-            var img = L.DomUtil.create('img');
-
-            img.src = '/images/help1.png';
-            img.addEventListener('click', function () {
-              openNav();
-              console.log("open nav...")
-            })
-
-            return img;
-        },
-    });
-
-    L.control.help = function(opts) {
-        return new L.Control.Help(opts);
+    function openNav() {
+        console.log("in openNav()...")
+        document.getElementById("myNav").style.height = "100%";
+        document.getElementById("myNav").style.width = "100%";
     }
 
-    L.control.help({ position: 'topright' }).addTo(map);
-
-  });
-
-  function openNav() {
-      console.log("in openNav()...")
-      document.getElementById("myNav").style.height = "100%";
-      document.getElementById("myNav").style.width = "100%";
-  }
-
-  function closeNav() {
-      document.getElementById("myNav").style.height = "0%";
-      document.getElementById("myNav").style.width = "0%";
-  }
+    function closeNav() {
+        document.getElementById("myNav").style.height = "0%";
+        document.getElementById("myNav").style.width = "0%";
+    }
+  } else {
+    alert("Server-Sent Events are not supported with your browser.")
+  };
 
 
 </script>
@@ -453,7 +457,7 @@ __END__
       </br></br>Display the latest earthquake on a map based on USGS information.
       </br></br>Note that earthquake information is not necessarily instantaneous meanings an earthquake that occurred 10 minutes ago may only be available through the USGS in the last few minutes.
       </br></br>Server-Sent Events are used in this webapp to send earthquake information from the server to the client (one-way; think twitter).  Please note that Server-Sent Events are not supported by IE/Edge at this writing. 
-      </br></br>This website is hosted at<a href="https://www.heroku.com/" target="_blank" class="heroku_text">Heroku</a>using a free dyno.
+      </br></br>This website is hosted at<a href="https://www.heroku.com/" target="_blank" style=color:yellow>Heroku</a>using a free dyno.
       </br></br>This website is for demonstration purposes.  Thanks for visiting.
       </br></br><a style=color:yellow href="https://github.com/greghorne/push_sinatra" target="_blank">GitHub Repository</a>
       </br></br></br><a href="https://www.ruby-lang.org" target="_blank"><img src="/images/Ruby_logo.svg" class="image_type1"></a>
