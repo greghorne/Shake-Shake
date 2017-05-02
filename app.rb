3999b64b820dee44fecd10736f6b0a4f44724921
Greg Horne <greghorne@hotmail.com>
2017-05-02 21:30:59 +0000

    added overlay panel

--- app.rb
+++ app.rb
@@ -186,6 +186,7 @@ __END__
         height: 96%;
         margin:  0;
         padding: 0;
+        z-index: 0;
       }
 
       .vertical-container {
@@ -206,6 +207,75 @@ __END__
         margin:  0;
         padding: 0;
       }
+
+      .image_type1 {
+        height: 42px;
+        width: 42px;
+      }
+
+      .image_type2 {
+        height: 42px;
+        width: 42px;
+      }
+
+      .image_type3 {
+        height: 42px;
+        width: 126px;
+      }
+
+    .overlay {
+        height: 100%;
+        width: 100%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        /*background-color: rgb(241,241,241);*/
+        background-color: rgba(181,152,152, 0.8);
+        overflow-x: hidden;
+        transition: 1s;
+        font-family: 'Lato', sans-serif;
+    }
+
+    .overlay-content {
+        position: relative;
+        top: 5%;
+        width: 100%;
+        text-align: center;
+        margin-top: 30px;
+        color: white;
+        /*margin: 10px, 20px, 10px, 20px;*/
+        /*padding: 20px 10px 20px 10px;*/
+    }
+
+    .overlay a {
+        padding: 8px;
+        text-decoration: none;
+        /*font-size: 36px;*/
+        color: #686868;
+        /*display: block;*/
+        transition: 0.1s;
+    }
+
+    .overlay a:hover, .overlay a:focus {
+        color: #f1f1f1;
+    }
+
+    .overlay .closebtn {
+        position: absolute;
+        top: 20px;
+        right: 45px;
+        font-size: 60px;
+    }
+
+    @media screen and (max-height: 450px) {
+      .overlay a {font-size: 20px}
+      .overlay .closebtn {
+        font-size: 40px;
+        top: 15px;
+        right: 35px;
+      }
+    }
 
     </style>
   </head> 
@@ -344,7 +414,8 @@ __END__
 
             img.src = '/images/help1.png';
             img.addEventListener('click', function () {
-              console.log("msg from help...")
+              openNav();
+              console.log("open nav...")
             })
 
             return img;
@@ -359,10 +430,37 @@ __END__
 
   });
 
+  function openNav() {
+      console.log("in openNav()...")
+      document.getElementById("myNav").style.height = "100%";
+      document.getElementById("myNav").style.width = "100%";
+  }
+
+  function closeNav() {
+      document.getElementById("myNav").style.height = "0%";
+      document.getElementById("myNav").style.width = "0%";
+  }
+
+
 </script>
 
-<form>
+
+  <div id="myNav" class="overlay" onclick="closeNav();">
+    <a class="closebtn" onclick="closeNav();" style=cursor:pointer>&times;</a>
+    <div class="overlay-content"><h1>Shake-Shake</h1>
+    <img src="/images/earthquake.png" height="96" width="96">
+    </br></br>Display the latest earthquake on a map based on USGS information.
+    </br></br>Note that earthquake information is not necessarily instantaneous meanings an earthquake that occurred 10 minutes ago may only be available through the USGS in the last few minutes.
+    </br></br>Server-Sent Events are used in this webapp to send earthquake information from the server to the client (one-way).  Please note that Server-Sent Events are not supported by IE/Edge at this writing. 
+    </br></br>This website is hosted at<a href="https://www.heroku.com/" target="_blank" class="heroku_text">Heroku</a>using a free dyno.
+    </br></br>This website is for demonstration purposes.  Thanks for visiting.
+    </br></br><a style=color:yellow href="https://github.com/greghorne/push_sinatra" target="_blank">GitHub Repository</a>
+    </br></br></br><a href="https://www.ruby-lang.org" target="_blank"><img src="/images/Ruby_logo.svg" class="image_type1"></a>
+    &nbsp;&nbsp;<a href="https://www.javascript.com" target="_blank"><img src="/images/js2.png" class="image_type1"></a>
+    &nbsp;&nbsp;<a href="https://earthquake.usgs.gov/fdsnws/event/1/" target="_blank"><img src="/images/usgs.jpg" class="image_type2"></a>
+    &nbsp;&nbsp;<a href="http://leafletjs.com" target="_blank"><img src="/images/leaflet.png" class="image_type3"></a></div></div>  
+  
   <div id='map'></div>
   <div class="vertical-container"><div id="msg"></div></div>
-</form>
+
 
